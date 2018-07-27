352cf33bf7961d497273df21234e2808fa578dc9
Krasen Ilkov <sillimanite90@gmail.com>
2018-07-27 18:47:33 +0300

    added config files to deploy on firebase

--- src/store/storeConfig.js
+++ src/store/storeConfig.js
@@ -6,7 +6,7 @@ import reducers from './reducers';
 // Redux DevTools in Browser
 const composeEnhancers = process.env.NODE_ENV === 'development'
   ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose  // eslint-disable-line
-  : null;
+  : null || compose;
 
 const reduxStore = createStore(reducers, composeEnhancers(
   applyMiddleware(thunk),
